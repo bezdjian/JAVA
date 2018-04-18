eb5d18cddb4524ad271bf9cc57566af032ffc4aa
Harout Bezdjian <harout.bezdjian@xtractor.se>
2018-04-18 10:45:00 +0200

    Adding AppTest class for testing purposes.

---
+++ src/test/java/org/o7planning/Hibernate5/AppTest.java
@@ -0,0 +1,34 @@
+package org.o7planning.Hibernate5;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AppTest extends TestCase{
+
+    /**
+     *  Create the test case
+     * @param name, name of the test case.
+     */
+    public AppTest(String name){
+        super(name);
+
+        System.out.println("Before test");
+
+        System.out.println("After test");
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite(){
+        return new TestSuite(AppTest.class);
+    }
+
+    /**
+     * Rigourous Test..
+     */
+    public void testApp(){
+        assertTrue(true);
+    }
+}
