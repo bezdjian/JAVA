744d06bb9abe43554969ce9cfb2214af1b7d1871
Harout Bezdjian <bezdjian-370z@hotmail.com>
2019-03-31 11:41:46 +0200

    Adding spring tomcat starter to deploy and start the app in tomcat

--- src/main/java/com/cybercom/example/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/cybercom/example/springbootdemo/SpringbootDemoApplication.java
@@ -2,9 +2,10 @@ package com.cybercom.example.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootDemoApplication {
+public class SpringbootDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDemoApplication.class, args);
