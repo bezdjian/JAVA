1fb8c892ab5e11a47cf8f34040b19c79353164dd
Harout Bezdjian <harout.bezdjian@sbab.se>
2019-10-23 10:52:00 +0200

    Removing mvn default files/folder

--- pact-contract-consumer/src/test/java/com/barath/app/SaveInventoryConsumerTest.java
+++ pact-contract-consumer/src/test/java/com/barath/app/SaveInventoryConsumerTest.java
@@ -25,50 +25,38 @@ import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.model.PactSpecVersion;
 import au.com.dius.pact.model.RequestResponsePact;
 
+public class SaveInventoryConsumerTest {
 
-public class SaveInventoryConsumerTest{
-	
     @Rule
-    public PactProviderRuleMk2 mockProvider = new PactProviderRuleMk2("inventory_provider",PactSpecVersion.V3, this);
-    private RestTemplate restTemplate=new RestTemplate();
-
+    public PactProviderRuleMk2 mockProvider = new PactProviderRuleMk2("inventory_provider", PactSpecVersion.V3, this);
+    private RestTemplate restTemplate = new RestTemplate();
 
     @Pact(provider = "inventory_provider", consumer = "inventory_consumer")
     public RequestResponsePact createPact(PactDslWithProvider builder) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", MediaType.APPLICATION_JSON_UTF8_VALUE);
 
+        PactDslJsonBody bodyResponse = new PactDslJsonBody().stringValue("productName", "TV")
+                .stringType("locationName", "CHENNAI").integerType("quantity", 100);
 
-        PactDslJsonBody bodyResponse = new PactDslJsonBody()
-                .stringValue("productName", "TV")
-                 .stringType("locationName", "CHENNAI")               
-                .integerType("quantity", 100);
-
-        return builder.given("create inventory").uponReceiving("a request to save inventory")
-                .path("/api/inventory")
-                .body(bodyResponse)
-                .method(RequestMethod.POST.name())
-                .willRespondWith()
-                .headers(headers)
-                .status(200).body(bodyResponse).toPact();
+        return builder.given("create inventory").uponReceiving("a request to save inventory").path("/api/inventory")
+                .body(bodyResponse).method(RequestMethod.POST.name()).willRespondWith().headers(headers).status(200)
+                .body(bodyResponse).toPact();
     }
 
-   
+    @Test
+    @PactVerification
+    public void testCreateInventoryConsumer() throws IOException {
 
-	
-	
-	@Test
-	@PactVerification
-	public void testCreateInventoryConsumer() throws IOException {
-		
-		Inventory inventory=new Inventory("TV", "CHENNAI", 100);
-    	HttpHeaders headers=new HttpHeaders();
-    	headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
-    	HttpEntity<Object> request=new HttpEntity<Object>(inventory, headers);
-    	ResponseEntity<String> responseEntity=restTemplate.postForEntity(mockProvider.getUrl()+"/api/inventory", request, String.class);
-    	assertEquals("TV", JsonPath.read(responseEntity.getBody(),"$.productName"));
-    	assertEquals("CHENNAI", JsonPath.read(responseEntity.getBody(),"$.locationName"));
-    	assertEquals((Integer)100, (Integer)JsonPath.read(responseEntity.getBody(),"$.quantity"));
-	}
+        Inventory inventory = new Inventory("TV", "CHENNAI", 100);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+        HttpEntity<Object> request = new HttpEntity<Object>(inventory, headers);
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(mockProvider.getUrl() + "/api/inventory",
+                request, String.class);
+        assertEquals("TV", JsonPath.read(responseEntity.getBody(), "$.productName"));
+        assertEquals("CHENNAI", JsonPath.read(responseEntity.getBody(), "$.locationName"));
+        assertEquals((Integer) 100, (Integer) JsonPath.read(responseEntity.getBody(), "$.quantity"));
+    }
 
 }
